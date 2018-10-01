27123c5784a9aa66b63eb2f4c5148be1bc632106
Chihurumnanya <nwanevunkem.chihurumnanya@gmail.com>
2018-10-01 12:59:15 +0100

    Updated Room and Data Persistence

---
+++ app/src/main/java/com/example/chihurmnanyanwanevu/popularmovies/model/MiniMovie.java
@@ -0,0 +1,5 @@
+package com.example.chihurmnanyanwanevu.popularmovies.model;
+
+public class MiniMovie {
+    public String movieId;
+}
---
+++ app/src/main/java/com/example/chihurmnanyanwanevu/popularmovies/ui/AboutActivity.java
@@ -0,0 +1,38 @@
+package com.example.chihurmnanyanwanevu.popularmovies.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.chihurmnanyanwanevu.popularmovies.R;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Element versionElement = new Element();
+        versionElement.setTitle("Version 1.2");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.ic_themoviedb)
+                .setDescription(getString(R.string.description))
+                .addItem(versionElement)
+                .addEmail("ahmetturk.pc@gmail.com")
+                .addWebsite("http://www.ahmetroid.com/")
+                .addPlayStore("com.ahmetroid.popularmovies")
+                .addGitHub("ahmetturk")
+                .create();
+
+        setContentView(aboutPage);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
